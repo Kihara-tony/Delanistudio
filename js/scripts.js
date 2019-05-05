b36b401eae53e06a7bd2444073183ca0f63a48c9
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-05-05 23:03:59 +0300

    added javascript content of the What we do part and the submit part in the scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#one").click(function(){
+    $(".design").toggle();
+    $(".Design").toggle();
+  });
+
+  $("#three").click(function(){
+    $(".management").toggle();
+    $(".manage").toggle();
+  });
+
+  $("#two").click(function(){
+    $(".development").toggle();
+    $(".develop").toggle();
+  });
+
+  $("#submit").click(function(){
+    var name =(document.getElementById("name").value);
+    alert( name +" thank you for contacting us.we received your message. :-).");
+  })
+});
