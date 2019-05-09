e333d29e329c6245ffedee4435e1313a235135ba
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-05-09 09:53:24 +0300

    added javascript of the hover effect on the portfolio images.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,16 @@ $(document).ready(function(){
     alert( name +" thank you for contacting us.we received your message. :-).");
   })
 });
+var item = document.getElementById("image");
+item.addEventListener("mouseover", func, false);
+item.addEventListener("mouseout", func1, false);
+
+function func()
+{
+   document.getElementById("text").setAttribute("style", "display:block;")
+}
+
+function func1()
+{
+    document.getElementById("text").setAttribute("style", "display:none;")
+}
